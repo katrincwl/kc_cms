66b6bc83b08c8c0d17b7e7e3c79b3f6d1474a020
Mark Luk <katrin.cwl@gmail.com>
2015-10-23 19:20:42 +0800

    Remove faker include to fix exception

--- application/controllers/Faker.php
+++ application/controllers/Faker.php
@@ -1,7 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-require_once '/vendor/fzaninotto/faker/src/autoload.php';
-
+defined('BASEPATH') OR exit('No direct script access allowed'); 
 /**
  * Author: Mark
  */
